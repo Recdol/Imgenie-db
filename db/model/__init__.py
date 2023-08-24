19079d14c59756656f974d4d5f3a9ffeb8531849
2jun0 <soo28819@gmail.com>
2023-08-24 15:16:21 +0900

    IMR-185 feat: add Inference Document

--- db/model/__init__.py
+++ db/model/__init__.py
@@ -4,5 +4,6 @@ from .playlist import Playlist
 from .song import Song
 from .user import User
 from .auth import Auth
+from .inference import Inference, InferenceQuery, InferenceOutput, InferenceFeedback
 
-__all__ = ["Album", "Artist", "Playlist", "Song", "User", "Auth"]
+__all__ = ["Album", "Artist", "Playlist", "Song", "User", "Auth", "Inference", "InferenceQuery", "InferenceOutput", "InferenceFeedback"]
